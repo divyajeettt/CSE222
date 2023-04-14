1d62bb2f3fea4616c961720caa4f2f4ad45877a4
divyajeettt <divyajeet21529@iiitd.ac.in>
2023-04-14 17:39:29 +0530

    Finished C

--- Lab-2/C.cpp
+++ Lab-2/C.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 
 #define ll long long
+#define pill pair<int, ll>
 #define till tuple<int, ll, ll>
+#define TIME(t) (t + Y/(t+1) + X)
 
 using namespace std;
 
 
 ll times[1000000];
-
-bool compare(int below, int above) { return times[below] > times[above]; }
 
 int main() {
     int N, M;
@@ -23,27 +23,29 @@ int main() {
         if (U != V) graph[V-1].push_back({U-1, X, Y});
     }
 
-    ll t[N] = { LONG_LONG_MAX };
-    priority_queue<int, vector<int>, function<bool(int, int)>> heap(compare);
-    for (int i = 0; i < N; i++) {
-        times[i] = LONG_LONG_MAX;
-        heap.push(i);
-    }
-    times[0] = t[0] = 0;
+    bool visited[N] = { false };
+    priority_queue<pill, vector<pill>, function<bool(pill, pill)>> heap(
+        [](pill a, pill b) { return a.second > b.second; }
+    );
+
+    for (int i = 0; i < N; i++) times[i] = LONG_LONG_MAX;
+    heap.push({0, 0});
+    times[0] = 0;
 
     while (!heap.empty()) {
-        int node = heap.top();
+        int node = heap.top().first;
         heap.pop();
-        if (t[node] > times[node]) continue;
+        if (visited[node]) continue;
+        visited[node] = true;
         for (till edge : graph[node]) {
             int child = get<0>(edge);
+            if (visited[child]) continue;
             ll X = get<1>(edge);
             ll Y = get<2>(edge);
-            ll opt_t = (Y == 0) ? times[node] : max(times[node], (ll) ceil(sqrt(Y) - 1));
-            ll new_time = (opt_t*(opt_t+1) + Y + X*(opt_t+1))/(opt_t+1);
-            if (times[child] > new_time) {
-                times[child] = new_time;
-                t[child] = opt_t;
+            ll opt_t = max(times[node], (ll) ceil(sqrt(Y)-1));
+            if (times[child] > TIME(opt_t)) {
+                times[child] = TIME(opt_t);
+                heap.push({child, TIME(opt_t)});
             }
         }
     }
